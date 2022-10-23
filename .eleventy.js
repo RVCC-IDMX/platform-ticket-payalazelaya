ae519ccbb8be0e765b4225074f661e570b986e13
Paola Ayala <paolaaz235@gmail.com>
2022-10-23 17:51:00 -0400

    Font styling and fixed how to get fonts

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
 // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addWatchTarget("./src/**/*.html");
   eleventyConfig.addWatchTarget("./src/**/*.js");
